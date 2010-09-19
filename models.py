13f7cc9fc3a8974278a46e35e1b2f1734cda60b3
casnerd <casnerd@828db145-3621-0410-a52e-dbbbbb52217e>
2010-09-19 01:04:24 +0000

    First cut at database schema and insertion from uploads.
    git-svn-id: file:///Users/mirandan/svn/torque@421 828db145-3621-0410-a52e-dbbbbb52217e

--- models.py
+++ models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Sample(models.Model):
+    time = models.DateTimeField(editable=False,
+                                help_text='Timestamp for the sample')
+    latitude = models.DecimalField(max_digits=16, decimal_places=13, null=True, blank=True, editable=False,
+                                   help_text='Location, latidute of sample')
+    longitude = models.DecimalField(max_digits=16, decimal_places=13, null=True, blank=True, editable=False,
+                                    help_text='Location, longitude of sample')
+    altitude = models.IntegerField(null=True, blank=True, editable=False,
+                                   help_text='Location, altitude of sample')
+    mpg = models.DecimalField(max_digits=6, decimal_places=3, editable=False, verbose_name='Miles per Gallon',
+                              help_text='Reported fuel efficiency in miles/gallon.')
+    throttle = models.DecimalField(max_digits=11, decimal_places=8, editable=False,
+                                   help_text='Throttle %')
+    rpm = models.IntegerField(blank=True, null=True, editable=False, verbose_name='engine revolutions per minute',
+                              help_text='Engine speed (RPM)')
+    speed = models.PositiveSmallIntegerField(blank=True, editable=False,
+                                             help_text='Vehicle speed (KPH)')
+    engine_temp = models.PositiveSmallIntegerField(editable=False, verbose_name='engine coolent temperature',
+                                                   help_text='Engine coolent temperature (C)')
+    vehicle_id = models.IntegerField(null=True, blank=True,
+                                     help_text='Vehigle identifier for this record.')
 
+    
--- views.py
+++ views.py
@@ -2,15 +2,40 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django import forms
 from djapps.torque.models import *
-import cPickle as pickle
+from datetime import datetime
+from decimal import Decimal
 
 def home(request):
+    return render_to_response('danielcasner/base.html', {'title': 'Torque', 'string_content':'Nothing to see here yet'})
     return HttpResponse('Nothing to see here yet.')
 
 def upload(request):
-    log = file('/home/www-data/djapps/torque/log.pickle', 'a')
-    if request.method == 'POST':
-        pickle.dump(('POST', request.POST), log, 0)
-    elif request.method == 'GET':
-        pickle.dump(('GET', request.GET), log, 0)
+    try:
+        time = datetime.fromtimestamp(float(request.GET['time'])/1000.0)
+        lat  = request.GET.get('kff1006')
+        if lat: lat = Decimal(lat)
+        lon  = request.GET.get('kff1005')
+        if lon: lon = Decimal(lon)
+        #altitude = ???
+        mpg  = Decimal(request.GET['kff1201'])
+        throttle = Decimal(request.GET['k11'])
+        speed = Decimal(request.GET['kd'])
+        rpm = request.GET.get('kc')
+        if rpm: rpm = Decimal(rpm)
+        etmp = request.GET.get('k5')
+        if etmp: etmp = Decimal(etmp)
+        vid = 0 # Where could I get this?
+    except Exception, inst:
+        errlog = file('upload_errors.log', 'a')
+        errlog.write('Sample error:\n\t%s\n\t%s\n\n' % (inst, repr(request.GET)))
+        errlog.close()
+    else:
+        Sample.objects.create(time=time,
+                              latitude=lat,
+                              longitude=lon,
+                              mpg=mpg,
+                              throttle=throttle,
+                              rpm=rpm,
+                              engine_temp=etmp,
+                              vehicle_id=vid)
     return HttpResponse('OK!')
