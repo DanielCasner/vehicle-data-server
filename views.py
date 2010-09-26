3e0d2bce5dd5540460b564c7f7964f895da1576b
casnerd <casnerd@828db145-3621-0410-a52e-dbbbbb52217e>
2010-09-26 16:18:52 +0000

    More, better filtered data.
    git-svn-id: file:///Users/mirandan/svn/torque@424 828db145-3621-0410-a52e-dbbbbb52217e

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django import forms
 from django.db.models import Avg, Max
 from djapps.torque.models import *
-from datetime import datetime
+from datetime import *
 from decimal import Decimal
 
 def home(request):
@@ -13,11 +13,11 @@ def home(request):
 
 def car_data(request):
     def fmt_mpg_vs_speed(querry):
-        return [[int(s['mpg']), s['speed']] for s in querry.values('mpg', 'speed')]
+        return [[int(mpg), speed] for mpg, speed in querry.values_list('mpg', 'speed')]
 
     jsvars = []
     
-    today = Sample.objects.order_by('time').reverse()[0].time.date()
+    today = Sample.objects.latest('time').time.date()
     jsvars.append('var today="' + today.isoformat() + '"')
 
     throttle_cutoff = Decimal('1.0')
@@ -27,14 +27,25 @@ def car_data(request):
     jsvars.append('var todays_mpg_vs_kph_coast=' + repr(fmt_mpg_vs_speed(todays_samples.filter(throttle__lte=throttle_cutoff))))
 
     old_samples = Sample.objects.filter(time__lt=today)
-    old_len = 10000
-    jsvars.append('var old_mpg_vs_kph_accel=' + repr(fmt_mpg_vs_speed(old_samples.filter(throttle__gt =throttle_cutoff)[:old_len])))
-    jsvars.append('var old_mpg_vs_kph_coast=' + repr(fmt_mpg_vs_speed(old_samples.filter(throttle__lte=throttle_cutoff)[:old_len])))
+    old_len = 5000
+    jsvars.append('var old_mpg_vs_kph_accel=' + repr(fmt_mpg_vs_speed(old_samples.filter(throttle__gt =throttle_cutoff).order_by('?')[:old_len])))
+    jsvars.append('var old_mpg_vs_kph_coast=' + repr(fmt_mpg_vs_speed(old_samples.filter(throttle__lte=throttle_cutoff).order_by('?')[:old_len])))
 
-    #two_months_ago = today - timedelta(60,0,0)
-    #last_two_months = Sample.objects.filter(time__gte=two_months_ago)
-    #two_months_mpg = last_two_months.values('time').annotate(mpg_avg=Avg('mpg'))
-    
+    two_months_ago = today - timedelta(61,0,0)
+    oneday = timedelta(1,0,0)
+    history = []
+    hi = 0
+    qday = today
+    while qday > two_months_ago:
+        samples = Sample.objects.filter(time__gte=qday, time__lt=qday+oneday).values('vehicle_id').annotate(mpg_avg=Avg('mpg'), temp_max=Max('engine_temp')).filter(vehicle_id=0)
+        if len(samples):
+            ag = samples[0]
+            history.append([hi, ag['mpg_avg'], ag['temp_max']])
+        hi -= 1
+        qday -= oneday
+
+    jsvars.append('var trend_data=' + repr(history))
+        
     jsvars.append('')
     return HttpResponse(';\n'.join(jsvars))
 
